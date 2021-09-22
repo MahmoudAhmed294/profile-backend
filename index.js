df12f958263af50c03e56893e149bd81cefcded6
MahmoudAhmed294 <60276598+MahmoudAhmed294@users.noreply.github.com>
2021-09-22 21:21:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,10 @@ var corsOptions = {
     origin: 'https://mahmoud-resume.netlify.app',
     optionsSuccessStatus: 200, // For legacy browser support,
     methods: ['GET', 'PUT', 'POST'],
-    allowedHeaders:['Content-Type', 'Authorization']
+    allowedHeaders:['Content-Type', 'Authorization'],
+        exposedHeaders:['Content-Range', 'X-Content-Range'],
+    credentials:true
+
 }
 
 app.use(cors(corsOptions));
